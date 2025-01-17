854a8351163263e6675fac86961a061f622b25df
Teeranai Sangtaera <151302226+SolElternal@users.noreply.github.com>
2025-01-17 23:40:13 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button ImageShapening;
+            System.Windows.Forms.Button MaxFilter;
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.OPENFOLDER = new System.Windows.Forms.Button();
+            ImageShapening = new System.Windows.Forms.Button();
+            MaxFilter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(265, 402);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Image Smoothing";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // ImageShapening
+            // 
+            ImageShapening.Location = new System.Drawing.Point(420, 402);
+            ImageShapening.Name = "ImageShapening";
+            ImageShapening.Size = new System.Drawing.Size(118, 59);
+            ImageShapening.TabIndex = 1;
+            ImageShapening.Text = "Image Shapening";
+            ImageShapening.UseVisualStyleBackColor = true;
+            ImageShapening.Click += new System.EventHandler(this.ImageShapening_Click);
+            // 
+            // MaxFilter
+            // 
+            MaxFilter.Location = new System.Drawing.Point(575, 402);
+            MaxFilter.Name = "MaxFilter";
+            MaxFilter.Size = new System.Drawing.Size(118, 59);
+            MaxFilter.TabIndex = 2;
+            MaxFilter.Text = "MAX Filter";
+            MaxFilter.UseVisualStyleBackColor = true;
+            MaxFilter.Click += new System.EventHandler(this.MaxFilter_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(56, 89);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(390, 269);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(512, 89);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(390, 269);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            this.openFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog2_FileOk);
+            // 
+            // OPENFOLDER
+            // 
+            this.OPENFOLDER.Location = new System.Drawing.Point(396, 503);
+            this.OPENFOLDER.Name = "OPENFOLDER";
+            this.OPENFOLDER.Size = new System.Drawing.Size(164, 47);
+            this.OPENFOLDER.TabIndex = 5;
+            this.OPENFOLDER.Text = "OPEN Folder";
+            this.OPENFOLDER.UseVisualStyleBackColor = true;
+            this.OPENFOLDER.Click += new System.EventHandler(this.OPENFOLDER_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(976, 590);
+            this.Controls.Add(this.OPENFOLDER);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(MaxFilter);
+            this.Controls.Add(ImageShapening);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button OPENFOLDER;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap f_image = null;  //input image
+        public Bitmap image;       //output image
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Null
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //Image Smoothing
+            Bitmap smoothedImage = new Bitmap(image.Width, image.Height);
+
+            int[,] filter = new int[3, 3]
+            {
+        { 1, 1, 1 },
+        { 1, 1, 1 },
+        { 1, 1, 1 }
+            };
+
+            int filterWeight = 9;
+
+            for (int x = 1; x < image.Width - 1; x++)
+            {
+                for (int y = 1; y < image.Height - 1; y++)
+                {
+                    int sum = 0;
+
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            sum += image.GetPixel(x + i, y + j).R * filter[i + 1, j + 1];
+                        }
+                    }
+
+                    int newGray = sum / filterWeight;
+                    smoothedImage.SetPixel(x, y, Color.FromArgb(newGray, newGray, newGray));
+                }
+            }
+
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    if (x == 0 || y == 0 || x == image.Width - 1 || y == image.Height - 1)
+                    {
+                        smoothedImage.SetPixel(x, y, image.GetPixel(x, y));
+                    }
+                }
+            }
+
+            pictureBox2.Image = smoothedImage;
+        }
+
+        private int Clamp(int value, int min, int max)
+        {
+            if (value < min) return min;
+            if (value > max) return max;
+            return value;
+        }
+
+        private void ImageShapening_Click(object sender, EventArgs e)
+        {
+            //Image Image Shapening
+            Bitmap sharpenedImage = new Bitmap(image.Width, image.Height);
+
+            int[,] filter = new int[3, 3]
+            {
+        { 1,  1,  1 },
+        { 1, -8,  1 },
+        { 1,  1,  1 }
+            };
+
+            for (int x = 1; x < image.Width - 1; x++)
+            {
+                for (int y = 1; y < image.Height - 1; y++)
+                {
+                    int sum = 0;
+
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            sum += image.GetPixel(x + i, y + j).R * filter[i + 1, j + 1];
+                        }
+                    }
+
+                    int newGray = Clamp(sum, 0, 255);
+                    sharpenedImage.SetPixel(x, y, Color.FromArgb(newGray, newGray, newGray));
+                }
+            }
+
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    if (x == 0 || y == 0 || x == image.Width - 1 || y == image.Height - 1)
+                    {
+                        sharpenedImage.SetPixel(x, y, image.GetPixel(x, y));
+                    }
+                }
+            }
+
+            pictureBox2.Image = sharpenedImage;
+        }
+
+        private void MaxFilter_Click(object sender, EventArgs e)
+        {
+            //Max Filter
+            Bitmap maxFilteredImage = new Bitmap(image.Width, image.Height);
+
+            // วนลูปสำหรับพิกเซลกลาง
+            for (int x = 1; x < image.Width - 1; x++)
+            {
+                for (int y = 1; y < image.Height - 1; y++)
+                {
+                    // สร้าง array เก็บค่า pixel 3x3
+                    int[] neighborhood = new int[9];
+                    int index = 0;
+
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            neighborhood[index++] = image.GetPixel(x + i, y + j).R;
+                        }
+                    }
+
+                    // หา Max value ใน neighborhood
+                    int maxGray = neighborhood.Max();
+
+                    maxFilteredImage.SetPixel(x, y, Color.FromArgb(maxGray, maxGray, maxGray));
+                }
+            }
+
+            // ตั้งค่าขอบภาพ (g(x, y) = f(x, y))
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    if (x == 0 || y == 0 || x == image.Width - 1 || y == image.Height - 1)
+                    {
+                        maxFilteredImage.SetPixel(x, y, image.GetPixel(x, y));
+                    }
+                }
+            }
+
+            pictureBox2.Image = maxFilteredImage;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //input image
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            //output image
+        }
+
+        private void OPENFOLDER_Click(object sender, EventArgs e)
+        {
+            // ใช้ OpenFileDialog เพื่อเลือกภาพ
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files (*.bmp;*.jpg;*.jpeg;*.png)|*.bmp;*.jpg;*.jpeg;*.png";
+                openFileDialog.Title = "Open Image File";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        // กำจัดภาพเก่าที่โหลดมาแล้ว
+                        if (f_image != null)
+                        {
+                            f_image.Dispose();
+                        }
+
+                        // โหลดภาพใหม่จากไฟล์
+                        f_image = new Bitmap(openFileDialog.FileName);
+
+                        // แปลงภาพเป็นระดับสีเทา
+                        image = new Bitmap(f_image.Width, f_image.Height);
+
+                        for (int i = 0; i < f_image.Width; i++)
+                        {
+                            for (int j = 0; j < f_image.Height; j++)
+                            {
+                                Color pixelColor = f_image.GetPixel(i, j);
+                                int c_gray = (int)(pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+                                image.SetPixel(i, j, Color.FromArgb(c_gray, c_gray, c_gray));
+                            }
+                        }
+
+                        // แสดงผลภาพใน PictureBox
+                        pictureBox1.Image = f_image;
+                        pictureBox2.Image = image;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Cannot open file: {ex.Message}", "File Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+            //not use
+        }
+    }
+}
